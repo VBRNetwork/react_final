09836bdc1a1c79058a025ecf717168a5558b245e
Vanea <stefan.liviu.vanea@hp.com>
2019-05-23 19:50:42 +0300

    setup done2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,10 @@
 import React, { PureComponent } from 'react'
-import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import ListGroup from 'react-bootstrap/ListGroup'
+
+
+import Row from 'react-bootstrap/Row'
+// import ListGroup from 'react-bootstrap/ListGroup'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -14,117 +17,17 @@ class Footer extends PureComponent {
 
         <footer className="footer-box" style={{ backgroundColor: '#37405E' }}>
           <div className='container'>
-            <Row>
-              <Col lg={3} xs={6}>
-                <ListGroup variant='flush'>
-                  <ListGroup.Item bsPrefix='footer-list'>
-                    <h5>Company</h5>
-                  </ListGroup.Item>
-                  <ListGroup.Item bsPrefix='footer-list'>
-                    <a href='/vbr-community'>VBR Community</a>
-                  </ListGroup.Item>
-                  <ListGroup.Item bsPrefix='footer-list'>
-                    <a href='/about-us'>About Us</a>
-                  </ListGroup.Item>
-                  <ListGroup.Item bsPrefix='footer-list'>
-                    <a href='/how-works'>How it works</a>
-                  </ListGroup.Item>
-                  <ListGroup.Item bsPrefix='footer-list'>
-                    <a href='/terms-and-condition'>Terms & Conditions</a>
-                  </ListGroup.Item>
-                  <ListGroup.Item bsPrefix='footer-list'>
-                    <a href='/vbr-community-defense'>VBR Community Defense</a>
-                  </ListGroup.Item>
-                  <ListGroup.Item bsPrefix='footer-list'>
-                    <a href='/terms-and-condition'>Privacy Policy</a>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Col>
-              <Col lg={3} xs={6}>
-                <ListGroup variant='flush'>
-                  <ListGroup.Item bsPrefix='footer-list'>
-                    <h5>Browse by location</h5>
-                  </ListGroup.Item>
-                  <ListGroup.Item bsPrefix='footer-list'>
-                    <a href='/vbr-community'>Europe</a>
-                  </ListGroup.Item>
-                  <ListGroup.Item bsPrefix='footer-list'>
-                    <a href='/about-us'>North America</a>
-                  </ListGroup.Item>
-                  <ListGroup.Item bsPrefix='footer-list'>
-                    <a href='/how-works'>Asia</a>
-                  </ListGroup.Item>
-                  <ListGroup.Item bsPrefix='footer-list'>
-                    <a href='/terms-and-condition'>South America</a>
-                  </ListGroup.Item>
-                  <ListGroup.Item bsPrefix='footer-list'>
-                    <a href='/terms-and-condition'>Australia</a>
-                  </ListGroup.Item>
-                  <ListGroup.Item bsPrefix='footer-list'>
-                    <a href='/vbr-community'>All locations</a>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Col>
-              <Col lg={3} xs={6}>
-                <ListGroup variant='flush'>
-                    <ListGroup.Item bsPrefix='footer-list'>
-                        <h5>Browse by skills</h5>
-                    </ListGroup.Item>
-                    <ListGroup.Item bsPrefix='footer-list'>
-                        <a href='/vbr-community'>Content Writing & Translation</a>
-                    </ListGroup.Item>
-                    <ListGroup.Item bsPrefix='footer-list'>
-                        <a href='/about-us'>Marketing & SEO</a>
-                    </ListGroup.Item>
-                    <ListGroup.Item bsPrefix='footer-list'>
-                        <a href='/how-works'>Graphic & Web Design</a>
-                    </ListGroup.Item>
-                    <ListGroup.Item bsPrefix='footer-list'>
-                        <a href='/terms-and-condition'>Business Development</a>
-                    </ListGroup.Item>
-                    <ListGroup.Item bsPrefix='footer-list'>
-                        <a href='/terms-and-condition'>Coding & Developers</a>
-                    </ListGroup.Item>
-                    <ListGroup.Item bsPrefix='footer-list'>
-                        <a href='/vbr-community'>All skills</a>
-                    </ListGroup.Item>
-                </ListGroup>
-              </Col>
-              <Col lg={3} xs={6}>
-                <ListGroup variant='flush'>
-                  <img
-                    src={'../../static/images/vbr_logo.png'}
-                    style={{ width: '120px', margin: '0 auto' }}
-                  />
-                  <Row style={{ marginTop: '30px' }}>
-                    <Col className='text-center text-white'>
-                      <a className='text-white' href=''>
-                        <FontAwesomeIcon icon={['fab', 'linkedin']} />
-                      </a>
-                    </Col>
-                    <Col className='text-center'>
-                      <a className='text-white' href=''>
-                        <FontAwesomeIcon icon={['fab', 'twitter']} />
-                      </a>
-                    </Col>
-                    <Col className='text-center'>
+            <Col className='text-center'>
                       <a className='text-white' href=''>
                         <FontAwesomeIcon icon={['fas', 'envelope-open']} />
                       </a>
-                    </Col>
-                  </Row>
-                </ListGroup>
-              </Col>
-            </Row>
-            <hr />
-            <Row>
-              <Col xs={12}>
-                <div className='text-center text-white copyright'>
-                  {' '}
-                        VBR Inc. 2019 All Rights Reserved.{' '}
-                </div>
-              </Col>
-            </Row>
+                      <a className='text-white' href=''>
+                        <FontAwesomeIcon icon={['fab', 'twitter']} />
+                      </a>
+                      <a className='text-white' href=''>
+                        <FontAwesomeIcon icon={['fab', 'linkedin']} />
+                      </a>
+            </Col>
           </div>
         </footer>
       )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,92 +1,17 @@
 import React, { PureComponent } from 'react'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import Button from 'react-bootstrap/Button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import '../styles/base.css'
+
+import Button from 'react-bootstrap/Button'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
 
 class Header extends PureComponent {
     render () {
         return (
             <div>
-                <Navbar expand='lg'>
-                    <div className='container'>
-                        <img src={'../static/images/vbr_logo.png'} style={{ width: '75px' }} />
-                        <Navbar.Toggle aria-controls='basic-navbar-nav'/>
-                            <Navbar.Collapse
-                            id='basic-navbar-nav'
-                            className='justify-content-end'>
-                                <Nav>
-                                    <Nav.Link href='/' className='small'>
-                                    <FontAwesomeIcon icon='home' size='sm' fixedWidth /> Home
-                                    </Nav.Link>
-                                    <Nav.Link href='#how-it-works' className='small'>
-                                        How it works
-                                    </Nav.Link>
-                                    <Nav.Link href='/#link' className='small'>
-                                        Community
-                                    </Nav.Link>
-                                    <Nav.Link href='/login' className='small'>
-                                        Login
-                                    </Nav.Link>
-                                    <Nav.Link href='/register' className='small'>
-                                        Register
-                                    </Nav.Link>
-                                    <div className="post-job-btn">
-                                        <Button
-                                            href='/postjob'
-                                            variant='success'
-                                            size='sm'
-                                            className='post-job-button'
-                                            style={{
-                                            backgroundColor: '#2EC3AB',
-                                            borderColor: '#2EC3AB'
-                                            }}>
-                                            Post a job
-                                        </Button>
-                                    </div>
-                                </Nav>
-                            </Navbar.Collapse>
-                    </div>
-                </Navbar>
-
-                <Navbar
-                expand='lg'
-                style={{
-                    borderBottom: '1px',
-                    paddingTop: '0px',
-                    boxShadow: ' 0px 1px 15px -3px #adadad',
-                    paddingBottom: '0px' }}>
-                
-                    <div className='container'>
-                        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-                            <Navbar.Collapse id='basic-navbar-nav'>
-                                <Nav>
-                                    <Nav.Link href='/writing' className='small mr-4'>
-                                    Writing
-                                    </Nav.Link>
-                                    <Nav.Link href='/marketing' className='small mr-4'>
-                                    Marketing & SEO
-                                    </Nav.Link>
-                                    <Nav.Link href='/design' className='small mr-4'>
-                                    Design
-                                    </Nav.Link>
-                                    <Nav.Link href='/businessconsultance' className='small mr-4'>
-                                    Consultance
-                                    </Nav.Link>
-                                    <Nav.Link href='/pr' className='small mr-4'>
-                                    PR
-                                    </Nav.Link>
-                                    <Nav.Link href='/developers' className='small mr-4'>
-                                    Developers
-                                    </Nav.Link>
-                                    <Nav.Link href='/allcategories' className='small mr-4'>
-                                    All Categories
-                                    </Nav.Link>
-                                </Nav>
-                            </Navbar.Collapse>
-                    </div>
-                </Navbar>
+                <FontAwesomeIcon icon='home' size='sm' fixedWidth /> Home
             </div>
         )
     }
