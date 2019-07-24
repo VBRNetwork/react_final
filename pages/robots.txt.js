f45f82d43179ad660fcbe69b04e42a838a6379ca
Vanea <stefan.liviu.vanea@hp.com>
2019-07-24 20:17:09 +0300

    added robots

---
+++ pages/robots.txt.js
@@ -0,0 +1,10 @@
+import React from 'react'
+export default class extends React.Component {
+  static async getInitialProps ({ req, res }) {
+    //res.writeHead(302, { Location: '/redirect' }) //sample how to response custom header
+    res.write('User-agent: *\n')
+    res.write('Disallow: /')
+    
+    res.end()
+  }
+}
