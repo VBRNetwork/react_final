39df9e01d0720e2f6769406dc6b3902c43aebdab
Vanea <stefan.liviu.vanea@hp.com>
2019-08-12 20:55:35 +0300

    add files

---
+++ src/containers/old_containers/DesignContainer.js
@@ -0,0 +1,107 @@
+import React, { Component, Fragment } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { Menu, Icon, Button, Avatar, Progress, Row, Card, Switch, Layout, List, Col, Statistic, Timeline, Dropdown, Rate, Typography, message } from 'antd'
+const { Meta } = Card
+const { SubMenu } = Menu
+const { Header, Content, Footer, Sider } = Layout
+const data = [
+  <a style={{ marginLeft: '5%', color: '#37405E' }} href='#'>Product Concept</a>,
+  <a style={{ marginLeft: '5%', color: '#37405E' }} href='#'>Graphic Design</a>,
+  <a style={{ marginLeft: '5%', color: '#37405E' }} href='#'>UI Design</a>,
+  <a style={{ marginLeft: '5%', color: '#37405E' }} href='#'>UX Design</a>,
+  <a style={{ marginLeft: '5%', color: '#37405E' }} href='#'>Search in all</a>
+]
+
+class DesignContainer extends Component {
+  static async getInitialProps ({ store, query }) {
+  }
+
+  componentDidMount () {
+  }
+
+  render () {
+    return (
+      <Layout>
+        <Sider style={{ backgroundColor: '#FFF' }}>
+          <Row>
+            <Col span={24}>
+              <div style={{ backgroundColor: '#FFF', marginLeft: '5%' }}><h3 style={{ margin: '16px 0' }}>Web & Graphic Design</h3></div>
+              <div>
+                <List
+                  style={{ backgroundColor: '#FFF' }}
+                  size='small'
+                  dataSource={data}
+                  renderItem={item => <List.Item>{item}</List.Item>}
+                />
+              </div>
+            </Col>
+          </Row>
+        </Sider>
+        <Content>
+          <Row gutter={40}>
+            <Col span={8} >
+              <Card
+                hoverable
+                style={{ marginTop: '5%', marginLeft: '10%', width: '90%', height: 'auto' }}
+                cover={<img alt='example' src='../static/images/designer_1.jpg' />}
+              >
+                <Meta title='Product Concept' />
+              </Card>
+            </Col>
+            <Col span={8} >
+              <Card
+                hoverable
+                style={{ marginTop: '5%', marginLeft: '5%', width: '90%', height: 'auto' }}
+                cover={<img alt='example' src='../static/images/designer_1.jpg' />}
+              >
+                <Meta title='Graphic Design' />
+              </Card>
+            </Col>
+            <Col span={8} >
+              <Card
+                hoverable
+                style={{ marginTop: '5%', width: '90%', height: 'auto' }}
+                cover={<img alt='example' src='../static/images/designer_1.jpg' />}
+              >
+                <Meta title='UI Design' />
+              </Card>
+            </Col>
+          </Row>
+          <Row gutter={40}>
+            <Col span={12}>
+              <Card
+                hoverable
+                style={{ marginBottom: '5%', marginTop: '15%', marginLeft: '20%', width: '58%', height: 'auto' }}
+                cover={<img alt='example' src='../static/images/designer_1.jpg' />}
+              >
+                <Meta title='UX Design' />
+              </Card>
+            </Col>
+            <Col span={12}>
+              <Card
+                hoverable
+                style={{ marginBottom: '5%', marginTop: '15%', marginLeft: '20%', width: '58%', height: 'auto' }}
+                cover={<img alt='example' src='../static/images/designer_1.jpg' />}
+              >
+                <Meta title='Search in all' />
+              </Card>
+            </Col>
+          </Row>
+        </Content>
+      </Layout>
+    )
+  }
+}
+
+function mapStateToProps (state) {
+  return {
+
+  }
+}
+
+DesignContainer.propTypes = {
+}
+
+export default connect(mapStateToProps, {
+})(DesignContainer)
