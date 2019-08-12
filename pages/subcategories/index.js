2d945abfdd614ca321581a72130900f3d0d5961a
Vanea <stefan.liviu.vanea@hp.com>
2019-08-12 20:59:00 +0300

    add files

--- pages/subcategories/index.js
+++ pages/subcategories/index.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react'
-import DevelopersContainer from 'containers/DevelopersContainer'
-import ConsultanceContainer from 'containers/ConsultanceContainer'
-import WritingContainer from 'containers/WritingContainer'
-import MarketingContainer from 'containers/MarketingContainer'
-import DesignContainer from 'containers/DesignContainer'
+
 import SearchJobsContainer from 'containers/jobs/SearchJobsContainer'
 
 export default class Index extends Component {
@@ -13,21 +9,6 @@ export default class Index extends Component {
 
   componentWillMount () {
     let category = this.props.router.query.category
-    if (category == 'developers') {
-      <DevelopersContainer jobs={[]} />
-    }
-    if (category == 'business-consultancy') {
-      return <ConsultanceContainer />
-    }
-    if (category == 'writing') {
-      return <WritingContainer />
-    }
-    if (category == 'marketing-and-seo') {
-      return <MarketingContainer />
-    }
-    if (category == 'design') {
-      return <DesignContainer />
-    }
     console.log(this.props.router)
   }
 
