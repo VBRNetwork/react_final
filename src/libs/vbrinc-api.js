b57fb30df919866c57b10fcfc066a50c340d9f99
vanea <stefan@vbrinc.ro>
2019-10-29 23:30:48 +0200

    update api url

--- src/libs/vbrinc-api.js
+++ src/libs/vbrinc-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import humps from 'humps'
-let apiUrl = 'https://marketplace.vbrinc.ro/api/v1/';
+let apiUrl = 'https://veelancing.io/api/v1/';
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     apiUrl = 'http://127.0.0.1:8000/api/v1/'
 }
