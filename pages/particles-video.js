8ab6cd0f81bbda728799bdea13576c6604652ace
vanea <root@vbrinc.ro>
2019-12-17 23:07:11 +0200

    created test pages for particles

---
+++ pages/particles-video.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import Particles from 'react-particles-js'
+import { Carousel, Layout, Row, Col, Button, Card, Input, Avatar, Empty, Breadcrumb, Form, Icon } from 'antd'
+
+
+
+export default class particles extends Component {
+
+  render () {
+    return (
+        <div className='container background-header' style={{height:window.innerHeight,position:'relative'}}>
+        <Particles
+            style={{ position: 'absolute' }}
+            params={{
+                particles: {
+                    number: {
+                        value: 270
+                    },
+                    size: {
+                        value: 3
+                    },
+                    color: { value: '#FFF' }
+                },
+                interactivity: {
+                    events: {
+                        onhover: {
+                            enable: true,
+                            mode: 'repulse'
+                        }
+                    }
+                }
+            }}>
+        </Particles>
+        </div>
+    )
+  }
+}
