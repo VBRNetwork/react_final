0096265022bbce53eb4debbcde852175ae04ab80
Serghei <serghei@vbrinc.ro>
2020-02-12 00:28:43 +0200

    add googletagmanager

---
+++ src/containers/PagesContainer.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { List, Avatar, Icon, Card, Layout, Menu, Row, Col, Checkbox } from 'antd'
+const { SubMenu } = Menu
+const { Content } = Layout
+
+
+const IconText = ({ type, text }) => (
+    <span>
+    <Icon type={type} style={{ marginRight: 8 }}/>
+        {text}
+  </span>
+)
+
+class PagesContainer extends Component {
+
+    constructor (props) {
+        super(props)
+        this.onChange = this.onChange.bind(this)
+    }
+
+    componentDidMount () {
+        window.gtag_report_conversion_employer()
+    }
+
+    onChange (e) {
+        console.log(`checked = ${e.target.checked}`)
+    }
+
+    render() {
+        return (
+            <Row>
+                <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={{ span: 17, offset: 3 }}>
+                    <Content style={{marginLeft:'1rem'}}>
+                        <Layout style={{ padding: '24px 0', background: '#fff' }}>
+                            <Row>
+                                {this.props.pageContent &&
+                                <Col  xs={24} sm={24} md={{span:7}} lg={{span:5}} xl={{span:5}} xxl={{ span: 5}}>
+                                    { this.props.pageContent.title && <div>{this.props.pageContent.title}</div>}
+                                    <br/>
+                                    {this.props.pageContent.content && <div>{this.props.pageContent.content}</div>}
+                                </Col>
+                                }
+                            </Row>
+
+                        </Layout>
+                    </Content>
+                </Col>
+            </Row>
+        )
+    }
+}
+
+function mapStateToProps (state) {
+    return {}
+}
+
+PagesContainer.propTypes = {}
+
+export default connect(mapStateToProps, {})(PagesContainer)
--- static/tagm.js
+++ static/tagm.js
@@ -1,6 +1,38 @@
 $( document ).ready(function() {
     window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments)}
+
+    function gtag () {dataLayer.push(arguments)}
+
     gtag('js', new Date());
     gtag('config', 'AW-699838440');
+
+    window.gtag_report_conversion_employer = function () {
+        gtag('event', 'conversion', {
+            'send_to': 'AW-699838440/NhQ6CLfmyMIBEOjf2s0C',
+        });
+        return false;
+    }
+    window.gtag_report_conversion_freelancer = function () {
+        gtag('event', 'conversion', {
+            'send_to': 'AW-699838440/NhQ6CLfmyMIBEOjf2s0C',
+        });
+        return false;
+    }
+
+    window.gtag_report_conversion_ico_lead = function () {
+        gtag('event', 'conversion', {
+            'send_to': 'AW-699838440/AM6jCIy32MIBEOjf2s0C',
+        });
+        return false;
+    }
+
+    window.gtag_report_conversion_ico_signup = function(){
+        gtag('event', 'conversion', {
+            'send_to': 'AW-699838440/95bcCKK82cIBEOjf2s0C',
+            'transaction_id': '',
+        });
+        return false;
+    }
+
+
 });
