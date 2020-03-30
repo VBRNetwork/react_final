f152b0b6063c483242f0014b12270dfc00367bca
Serghei <serghei@vbrinc.ro>
2020-03-31 00:33:05 +0300

    add referal system

--- src/containers/dashboard/ReferralsContainer.js
+++ src/containers/dashboard/ReferralsContainer.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Card, Col, Row, Alert} from 'antd'
-import Link from 'next/link'
-
 
 class ReferralsContainer extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            referral_code: 10,
-            payment_method:1,
         };
     }
 
     render () {
-        let link = 'https://veelancing.io/register?join=' + this.props.referral_code.referralCode
+        let link = 'https://ico.veelancing.io/register?join=' + this.props.referral_code.referralCode
         return (
             <div>
                 <Card title='Referral Members'>
                     <Row>
                         <Col xl={16}>
-                            Your referral link is <br/>
-                            <Link target={'_blank'} href={link}>{link}</Link>
+                            Your referral link is: <br/>
+                            <Alert message= {link}/>
                         </Col>
                     </Row>
                 </Card>
