ef4f511842bc71f70fb69abba5b42860b53a46b8
Neal Summers <root@vbrinc.ro>
2020-06-05 16:06:19 +0300

    updated vbrapi

--- src/libs/vbrinc-api.js
+++ src/libs/vbrinc-api.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 import humps from 'humps'
 
-let apiUrl = 'https://127.0.0.1:8000/api/v1/';
+let apiUrl = 'https://veelancing.io/api/v1/';
 if ((!process.env.NODE_ENV || process.env.NODE_ENV === false)
     //PUT false
     || false) {
-    apiUrl = 'http://127.0.0.1:8000/api/v1/'
+    apiUrl = 'https://127.0.0.1:8000/api/v1/'
 }
 
 if (typeof window !== 'undefined' && window.location && window.location.host === 'ico.veelancing.io') {
